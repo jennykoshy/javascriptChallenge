8ce8f215f2a13e514fd6fabe6d358ac638b63ce3
Jenny Koshy <rajanirvarghese@gmail.com>
2019-06-07 14:59:34 -0500

    Adding start text file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-let value = 0; 
+function bmiCalculator(weight, height) {
+    var bioMedicalIndex = Math.floor(weight/ (height*height));
+    return bioMedicalIndex;
+}
+var bmi = bmiCalculator(65, 1.8);
+console.log(bmi);
